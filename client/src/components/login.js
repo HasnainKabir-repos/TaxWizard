a3deab45ed61a5a7f57ca0dc052e16b6c6f4ffb5
Oishy Fatema Akhand <95131852+oishy28@users.noreply.github.com>
2023-11-03 11:54:04 +0600

    login frontend

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:8000//login",
+        "http://localhost:8000/login", // Remove extra slashes
         formData
       );
       if (response.data.token) {
@@ -21,7 +21,7 @@ function Login() {
           JSON.stringify(response.data.token)
         );
         console.log("Login successful!");
-        window.location = "//profile";
+        window.location = "/profile"; // Remove extra slashes
       } else {
         console.log("Login failed!");
       }
@@ -31,17 +31,32 @@ function Login() {
   };
 
   const styles = {
+    page: {
+      display: "flex",
+      justifyContent: "center", // Center horizontally
+      alignItems: "center", // Center vertically
+      height: "100vh", // Make the page take up the full viewport height
+    },
     container: {
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
       padding: "20px",
-      border: "1px solid #e5e5e5",
+      border: "1px solid white", // White border
       borderRadius: "5px",
-      boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.1)",
+      boxShadow: "0px 0px 8px white", // White shadow background
       maxWidth: "400px",
-      margin: "50px auto",
-      background: "#111111"
+      background: "#7CABA1", // Original background color
+    },
+    header: {
+      display: "flex",
+      flexDirection: "column", // Place logo and title in a column
+      alignItems: "center", // Center elements horizontally
+      marginBottom: "20px",
+    },
+    logo: {
+      width: "50px", // Adjust the width of the logo as needed
+      height: "auto",
     },
     input: {
       margin: "10px 0",
@@ -56,38 +71,49 @@ function Login() {
       borderRadius: "5px",
       border: "none",
       cursor: "pointer",
-      marginTop: "20px",
+      marginTop: "10px", // Adjust the margin between buttons
+      marginBottom: "10px", // Add margin at the bottom of the button
     },
     h2: {
       color: "white",
-      fontWeight: "bold"
+      fontWeight: "bold",
     },
   };
 
+
   return (
-    <div style={styles.container}>
-      <h2 style={styles.h2}> Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          style={styles.input}
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-          required
-        />
-        <input
-          style={styles.input}
-          type="password"
-          name="password"
-          placeholder="Password"
-          onChange={handleChange}
-          required
-        />
-        <button style={styles.button} type="submit">
-          Login
-        </button>
-      </form>
+    <div style={styles.page}>
+      <div style={styles.container}>
+        <div style={styles.header}>
+          {/* Add your logo here */}
+          <img src="/images/logo.png" alt="Logo" style={styles.logo} />
+          <h2 style={styles.h2}>Login</h2>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            style={styles.input}
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            required
+          />
+          <input
+            style={styles.input}
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            required
+          />
+          <button style={styles.button} type="submit">
+            Login
+          </button>
+          <button style={styles.button} type="button">
+            Sign Up
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
